890152f9110c15ed76f43688d64d06013f922a6b
Mike Pagé <mike@nedwave.com>
2013-05-29 12:12:42 +0200

    Add JMSSerializerBundle, FOSUserBundle, FOSOAuthServerBundle, MPRestApiBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,10 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+            new MP\RestApiBundle\MPRestApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
