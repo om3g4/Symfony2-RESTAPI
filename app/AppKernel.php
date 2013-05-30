d90ec064cb3b3cedd474ab4ddbf88cc378d80acb
Mike Pagé <mike@nedwave.com>
2013-05-30 13:43:15 +0200

    Add and config FOSRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
             new MP\RestApiBundle\MPRestApiBundle(),
         );
 
